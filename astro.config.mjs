3d1b89b272dac2fa523f6c8b61da2e63575793fb
sooox-cc <187102319+sooox-cc@users.noreply.github.com>
2026-02-28 17:57:36 +0100

    update: fonts, index, contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+    },
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+    ],
+  },
 
   vite: {
     plugins: [tailwindcss()]
